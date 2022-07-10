d85164aa11687872dd7cd4cd8073ea60cb525815
Görkem <gorkem_yelken@hotmail.com>
2022-07-10 14:27:16 +0300

    G gorke@G▒rkem-PC MINGW64 ~/Desktop/Masaüstü/KodluyoruzProjects (main) $ git add . warning: LF will be replaced by CRLF in teksayilarintoplaminibulanprogram/src/Main.java. The file will have its original line endings in your working directory
    gorke@G▒rkem-PC MINGW64 ~/Desktop/Masaüstü/KodluyoruzProjects (main)
    $ git add .
    
    gorke@G▒rkem-PC MINGW64 ~/Desktop/Masaüstü/KodluyoruzProjects (main)
    $ git commit -m

---
+++ teksayilarintoplaminibulanprogram/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int m , toplam = 0;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Sayi Gir: " );
+            m = input.nextInt();
+            if ( m % 2 == 0 && m % 4 == 0){
+
+                toplam +=m;
+
+            }
+
+        } while (m % 2 ==0);
+        System.out.println("Toplam= " + toplam);
+    }
+}
