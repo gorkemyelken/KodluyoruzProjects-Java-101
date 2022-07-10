0dd017c6611edcf4a539e90140d00e29b4ca2c52
Görkem <gorkem_yelken@hotmail.com>
2022-07-10 15:30:28 +0300

    Fibonacci Serisi

---
+++ fibonacciserisi/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr=new Scanner(System.in);
+        System.out.print("Basamak sayisini girin: ");
+        int n=scr.nextInt();
+        int b=0, c=1,d;
+        for (int i=1;i<=n;i++){
+            d=b+c;
+            System.out.println(d);
+            b=c;
+            c=d;
+
+            System.out.println(" ");
+        }
+    }
+}
